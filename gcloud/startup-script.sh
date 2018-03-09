d7e5549662b4d9a30de944958455bf95b00a23dd
NetworkingJoe <paulierev1775@gmail.com>
2018-03-08 19:54:40 -0800

    commented on startup-script

--- gcloud/startup-script.sh
+++ gcloud/startup-script.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
-echo "yaaaaah hoooooooooooo"
+#!/bin/bash                       #this basically says we are working in bash language and gets the code started at a user level.
+echo "yaaaaah hoooooooooooo"      #adds the comment "yaaaaah hoooooooooooo" too the top of the page              
 
-yum -y install httpd
-systemctl enable httpd
-systemctl start httpd
+yum -y install httpd              #gives a yes command to installing a basic version of apache.
+systemctl enable httpd            #this enables apache to run in our environment.
+systemctl start httpd             #this starts apache when the server is powered on.
