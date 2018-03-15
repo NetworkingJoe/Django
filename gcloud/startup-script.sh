afc5a39527b0948a5a5b2b168108bd7f5a4c4444
NetworkingJoe <paulierev1775@gmail.com>
2018-03-15 14:05:38 -0700

    added in comments for the file

--- gcloud/startup-script.sh
+++ gcloud/startup-script.sh
@@ -8,7 +8,6 @@ def django_setup():
   os.system('sudo mkdir /opt/django')
   os.chdir('/opt/django')
   os.system('sudo useradd paulierev1775')
-
   os.system('sudo yum install epel-release -y')
   os.system('sudo yum install python34 python-pip -y')
 
@@ -16,17 +15,16 @@ def django_install():
   os.system('sudo virtualenv -p python3 django')
   os.chdir('/opt/django/django')
   os.system('source /opt/django/django/bin/activate && pip install django')
-
   os.system('source /opt/django/django/bin/activate && django-admin startproject project1')
 
   os.chdir('..')
   os.system('sudo chown -R paulierev1775 /opt/django/')
   os.system('yum install git -y')
-
-
-
   os.system('myip=$( curl ifconfig.co ) && sed -i "s/ALLOWED_HOSTS = \[\]/ALLOWED_HOSTS =  \[\'"$myip"\'\]/g" /opt/django/django/project1/project1/settings.py ')
  
+# "This is my comment for the startupscript.sh file. I am commenting here to remind myself that although the line above does
+#  Indeed add the allowed hosts to the file, every time I start up the instance, I have to go in and plug in the new I.P. address
+#  In order to get it working properly. This is because my instance generates a new IP address every time."
 
 def django_start():
   os.system('sudo -u paulierev1775 virtualenv -p python3 django')
