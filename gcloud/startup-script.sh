048dda7d250650c56985eef2d126ae5d5648f09a
NetworkingJoe <paulierev1775@gmail.com>
2018-03-14 17:43:13 -0700

    made my django server automation my startup script

--- gcloud/startup-script.sh
+++ gcloud/startup-script.sh
@@ -1,6 +1,38 @@
-#!/bin/bash                       #this basically says we are working in bash language and gets the code started at a user level.
-echo "yaaaaah hoooooooooooo"      #adds the comment "yaaaaah hoooooooooooo" too the top of the page              
+#!/usr/bin/python
+import os
 
-yum -y install httpd              #gives a yes command to installing a basic version of apache.
-systemctl enable httpd            #this enables apache to run in our environment.
-systemctl start httpd             #this starts apache when the server is powered on.
+def django_setup():
+  os.system('sudo yum install python-pip -y')
+  os.system('sudo pip install virtualenv')
+  os.system('sudo pip install --upgrade pip')
+  os.system('sudo mkdir /opt/django')
+  os.chdir('/opt/django')
+  os.system('sudo useradd paulierev1775')
+
+  os.system('sudo yum install epel-release -y')
+  os.system('sudo yum install python34 python-pip -y')
+
+def django_install():
+  os.system('sudo virtualenv -p python3 django')
+  os.chdir('/opt/django/django')
+  os.system('source /opt/django/django/bin/activate && pip install django')
+
+  os.system('source /opt/django/django/bin/activate && django-admin startproject project1')
+
+  os.chdir('..')
+  os.system('sudo chown -R paulierev1775 /opt/django/')
+  os.system('yum install git -y')
+
+
+
+  os.system('myip=$( curl ifconfig.co ) && sed -i "s/ALLOWED_HOSTS = \[\]/ALLOWED_HOSTS =  \[\'"$myip"\'\]/g" /opt/django/django/project1/project1/settings.py ')
+ 
+
+def django_start():
+  os.system('sudo -u paulierev1775 virtualenv -p python3 django')
+  os.chdir('/opt/django/django')
+  os.system('sudo -u paulierev1775 -E sh -c "source /opt/django/django/bin/activate && /opt/django/django/project1/manage.py runserver 0.0.0.0:8000&"')
+
+django_setup()
+django_install()
+django_start()
